dd34b9c1b1b04885bdd17a01785f121782749b07
kerdlix <danielbgg@gmail.com>
2017-12-08 16:47:11 -0200

    Lecture 57

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,35 @@ const expect = require('expect');
 
 var app = require('./server.js').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
+describe('Server', () => {
 
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Daniel1',
-        age: 1
-      });
-    })
-    .end(done);
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', () => {
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Daniel1',
+            age: 1
+          });
+        })
+        .end(done);
+    });
+  });
+
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,58 +1,68 @@
 const expect = require('expect');;
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  expect(utils.add(33, 11)).toBe(44).toBeA('number');
-});
+describe('Utils', () => {
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
-  })
-});
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      expect(utils.add(33, 11)).toBe(44).toBeA('number');
+    });
 
-it('should square a number', () => {
-  expect(utils.square(9)).toBe(81).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(3, (square) => {
-    expect(square).toBe(9).toBeA('number');
-    done();
-  })
-});
-
-it('should expect some values', () => {
-  expect(12).toNotBe(11);
-  expect({name: 'Daniel'}).toEqual({name: 'Daniel'});
-  expect({name: 'daniel'}).toNotEqual({name: 'Daniel'});
-  expect([2,3,4]).toInclude(4);
-  expect([2,3,4]).toExclude(5);
-  expect({
-    name: 'Daniel',
-    age: 38,
-    location: 'Sao Paulo'
-  }).toInclude({
-    age: 38
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      })
+    });
   });
-  expect({
-    name: 'Daniel',
-    age: 38,
-    location: 'Sao Paulo'
-  }).toExclude({
-    age: 30
-  });
-});
 
-it('should set firstName and lastName', () => {
-  var user = {age: 38, location: 'Sao Paulo'};
-  user = utils.setName(user, 'Daniel Goncalves');
-  expect(user).toInclude({
-    firstName: 'Daniel',
-    lastName: 'Goncalves'
+  describe('#square', () => {
+    it('should square a number', () => {
+      expect(utils.square(9)).toBe(81).toBeA('number');
+    });
+
+    it('should async square a number', (done) => {
+      utils.asyncSquare(3, (square) => {
+        expect(square).toBe(9).toBeA('number');
+        done();
+      })
+    });
   });
-  expect("Daniel").toEqual(user.firstName);
-  expect("Goncalves").toEqual(user.lastName);
-  expect(user).toBeA('object');
+
+  describe('#others', () => {
+    it('should expect some values', () => {
+      expect(12).toNotBe(11);
+      expect({name: 'Daniel'}).toEqual({name: 'Daniel'});
+      expect({name: 'daniel'}).toNotEqual({name: 'Daniel'});
+      expect([2,3,4]).toInclude(4);
+      expect([2,3,4]).toExclude(5);
+      expect({
+        name: 'Daniel',
+        age: 38,
+        location: 'Sao Paulo'
+      }).toInclude({
+        age: 38
+      });
+      expect({
+        name: 'Daniel',
+        age: 38,
+        location: 'Sao Paulo'
+      }).toExclude({
+        age: 30
+      });
+    });
+
+    it('should set firstName and lastName', () => {
+      var user = {age: 38, location: 'Sao Paulo'};
+      user = utils.setName(user, 'Daniel Goncalves');
+      expect(user).toInclude({
+        firstName: 'Daniel',
+        lastName: 'Goncalves'
+      });
+      expect("Daniel").toEqual(user.firstName);
+      expect("Goncalves").toEqual(user.lastName);
+      expect(user).toBeA('object');
+    });
+  });
+
 });
