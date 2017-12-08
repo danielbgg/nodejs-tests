c3907de8a4635fbfff43041af4254538b4dd5039
kerdlix <danielbgg@gmail.com>
2017-12-08 16:38:42 -0200

    Lecture 56

--- server/server.js
+++ server/server.js
@@ -3,7 +3,25 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(404).send({
+  error: 'Page not found.',
+  name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.status(200).send({
+      name: 'Daniel1',
+      age: 1
+    }, {
+      name: 'Daniel2',
+      age: 2
+    }, {
+      name: 'Daniel3',
+      age: 3
+  });
 });
 
 app.listen(3000);
+
+module.exports.app = app;
